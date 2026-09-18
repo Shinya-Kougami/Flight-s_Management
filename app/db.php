1f25a2fd7f49fcc64074fbd693584cffdf6834c9
Shinya-Kougami <marvinloera@gmail.com>
2026-09-18 10:59:41 -0600

    Base de datos en php creada y vericada

---
+++ app/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'db';
+$db = 'vuelos_db';
+$user = 'root';
+$pass = 'root';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+?>
